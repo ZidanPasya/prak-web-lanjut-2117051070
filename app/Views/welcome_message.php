ed3f6af7ca26edf45b97fc39891dd4c5d9cf070e
ZidanPasya <zipas354@gmail.com>
2023-09-14 00:53:35 +0700

    Update Welcome Message

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -220,7 +220,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>Muhammad Zidan Pasya <br>2117051070</h1>
 
         <h2>The small framework with powerful features</h2>
 
