d44f1393195bac21cea4d2ca756f8cde74847e83
Muhammad Satria <mmmuhammadsatria@gmail.com>
2023-10-24 17:30:48 +0700

    profile

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -6,22 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    <table>
-        <tr>
-            <td>Nama</td>
-            <td>:</td>
-            <td><?= $nama ?></td>
-        </tr>
-        <tr>
-            <td>Kelas</td>
-            <td>:</td>
-            <td><?= $kelas ?></td>
-        </tr>
-        <tr>
-            <td>NPM</td>
-            <td>:</td>
-            <td><?= $npm ?></td>
-        </tr>
-    </table>
+    
 </body>
 </html>
